5388ba73ace407c4d5730d81bfbb5cf001feb747
ilizium <lvov.valerij@erkapharm.com>
2023-10-28 15:07:58 +0500

    brand, category, product

---
+++ database/migrations/2023_10_22_131423_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Brand;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('thumbnail')->nullable();
+            $table->unsignedBigInteger('price')->default(0);
+
+            $table->foreignIdFor(Brand::class)
+                ->nullable()
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if(app()->isLocal()) {
+            Schema::dropIfExists('products');
+        }
+    }
+};
